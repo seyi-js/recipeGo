188e9b3f15178d8e8daa39dbc2377807f2a42b3e
SAMUEL ADEBAYO OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2022-05-14 15:21:24 +0100

    changed name

--- android/app/src/main/java/com/recipego/MainApplication.java
+++ android/app/src/main/java/com/recipego/MainApplication.java
@@ -1,4 +1,4 @@
-package com.recipego;
+package com.recipeapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -67,7 +67,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.recipego.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.recipeapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
